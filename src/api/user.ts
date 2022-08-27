a061099c86e94438c2183dd0e762d89d1ae18a25
Yan Fu <fuyan@yans-mbp.student.wireless.sydney.edu.au>
2022-08-27 13:56:52 +1000

    post and signup

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,3 +3,7 @@ import request from '@/utils/request'
 export const getUserInfo = (params: object): Promise<any> => {
   return request('/login', 'post', params)
 }
+
+export const signUp = (params: object): Promise<any> => {
+  return request('/signup', 'post', params)
+}
---
+++ src/views/new/posts/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <van-uploader v-model="fileList" :after-read="afterRead" />
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const fileList = ref([
+      // {
+      //   url: 'https://fastly.jsdelivr.net/npm/@vant/assets/leaf.jpeg',
+      //   status: 'uploading',
+      //   message: 'Uploading...',
+      // },
+      // {
+      //   url: 'https://fastly.jsdelivr.net/npm/@vant/assets/tree.jpeg',
+      //   status: 'failed',
+      //   message: 'Failed',
+      // },
+    ])
+
+    const afterRead = (file: any) => {
+      file.status = 'uploading'
+      file.message = '上传中...'
+      console.log(file)
+      setTimeout(() => {
+        console.log(file)
+        file.status = 'failed'
+        file.message = '上传失败'
+      }, 1000)
+    }
+
+    return {
+      fileList,
+      afterRead,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
