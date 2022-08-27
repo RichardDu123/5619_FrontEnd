a785e2f3f0a328ae6300c6a1764053d2da670fc6
Yan Fu <fuyan@yans-mbp.student.wireless.sydney.edu.au>
2022-08-27 17:08:55 +1000

    implement AddPost Page

--- src/api/post.ts
+++ src/api/post.ts
@@ -3,3 +3,7 @@ import request from '@/utils/request'
 export const getPosts = (params: object): Promise<any> => {
   return request('/getPosts', 'get', params)
 }
+
+export const submitPosts = (params: object): Promise<any> => {
+  return request('/submitPosts', 'post', params)
+}
--- src/views/new/posts/index.vue
+++ src/views/new/posts/index.vue
@@ -1,44 +1,104 @@
 <template>
-  <div>
-    <van-uploader v-model="fileList" :after-read="afterRead" />
+  <div class="addPost">
+    <van-nav-bar
+      left-arrow
+      @click-left="onClickLeft"
+      title="Add Post"
+      class="myNav"
+      fixed
+    />
+    <div class="add-post-upload-image">
+      <van-uploader v-model="fileList" multiple :max-count="6" class="img" />
+    </div>
+    <van-cell-group inset>
+      <van-field v-model="title" placeholder="Add Title" />
+    </van-cell-group>
+    <van-divider></van-divider>
+
+    <van-cell-group inset>
+      <van-field
+        v-model="description"
+        rows="4"
+        autosize
+        type="textarea"
+        maxlength="100"
+        placeholder="Add text"
+        show-word-limit
+      />
+    </van-cell-group>
+    <div class="add-post-button">
+      <van-button
+        round
+        color="linear-gradient(to right, #ff6034, #ee0a24)"
+        class="postBtn"
+        @click="onClickSubmitPost"
+      >
+        Add Post
+      </van-button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { submitPosts } from '@/api/post'
 
 export default {
   setup() {
-    const fileList = ref([
-      // {
-      //   url: 'https://fastly.jsdelivr.net/npm/@vant/assets/leaf.jpeg',
-      //   status: 'uploading',
-      //   message: 'Uploading...',
-      // },
-      // {
-      //   url: 'https://fastly.jsdelivr.net/npm/@vant/assets/tree.jpeg',
-      //   status: 'failed',
-      //   message: 'Failed',
-      // },
-    ])
-
+    const fileList = ref([])
+    const title = ref('')
+    const description = ref('')
     const afterRead = (file: any) => {
-      file.status = 'uploading'
-      file.message = '上传中...'
+      // 此时可以自行将文件上传至服务器
       console.log(file)
-      setTimeout(() => {
-        console.log(file)
-        file.status = 'failed'
-        file.message = '上传失败'
-      }, 1000)
+    }
+    const router = useRouter()
+    const onClickLeft = () => {
+      router.back()
+    }
+
+    const onClickSubmitPost = () => {
+      submitPosts({
+        title: title.value,
+        postContent: description.value,
+        imageUrlList: fileList.value,
+      }).then((value) => {
+        console.log(value)
+      })
     }
 
     return {
+      title,
+      description,
       fileList,
       afterRead,
+      onClickLeft,
+      onClickSubmitPost,
     }
   },
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.addPost {
+  padding-top: 50px;
+  .add-post-upload-image {
+    margin-top: 15px;
+    margin-left: 15px;
+    margin-bottom: 5px;
+    display: flex;
+    justify-content: space-evenly;
+    .img {
+      flex: 1;
+    }
+  }
+}
+.add-post-button {
+  padding-top: 30px;
+  text-align: center;
+  .postBtn {
+    width: 300px;
+  }
+}
+</style>
