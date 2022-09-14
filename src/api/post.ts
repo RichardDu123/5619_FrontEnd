4183570d43f3b14725fb7a765617eb8efd3ce06a
Yan Fu <fuyan@192-168-1-108.tpgi.com.au>
2022-09-14 21:19:57 +1000

    fix router issue

--- src/api/post.ts
+++ src/api/post.ts
@@ -8,6 +8,10 @@ export const submitPosts = (params: object): Promise<any> => {
   return request('/post/newPost', 'post', params)
 }
 
+export const createPet = (params: object): Promise<any> => {
+  return request('/pet/newPet', 'post', params)
+}
+
 export const getImage = (params: object): Promise<any> => {
   return request('/getImage', 'get', params)
 }
